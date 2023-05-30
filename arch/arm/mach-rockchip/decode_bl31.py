67674926c474871dc4780c49138e3772e66cc5c2
UtsavBalar1231 <utsavbalar1231@gmail.com>
2023-05-30 12:17:16 +0530

    Revert "ARM: mach-rockchip: Replace python2 with python"
    This reverts commit db85b84afd621baf479e78e3ab310d13f3b796ef.

--- arch/arm/mach-rockchip/decode_bl31.py
+++ arch/arm/mach-rockchip/decode_bl31.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 #
 # Copyright (C) 2020 Rockchip Electronics Co., Ltd
 #
--- arch/arm/mach-rockchip/make_fit_atf.py
+++ arch/arm/mach-rockchip/make_fit_atf.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 """
 A script to generate FIT image source for rockchip boards
 with ARM Trusted Firmware
